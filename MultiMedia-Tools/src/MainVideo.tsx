dd3cfffda9ed63e36e56418d289d5be0c2890f02
Mr_Sure21 <52558137+omaralian98@users.noreply.github.com>
2025-10-20 08:35:25 +0300

    Added fading effect to the music of the video

--- MultiMedia-Tools/src/MainVideo.tsx
+++ MultiMedia-Tools/src/MainVideo.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import {AbsoluteFill, Html5Audio, Sequence, staticFile, useVideoConfig} from 'remotion';
+import {
+    AbsoluteFill,
+    Html5Audio,
+    Sequence,
+    staticFile,
+    useVideoConfig,
+    useCurrentFrame,
+    interpolate,
+} from 'remotion';
 import Grid from './components/Grid';
-import {PresentationLogo} from "./components/PresentationLogo";
-import {Durations} from "./Root";
-import {FadeToBlack} from "./components/FadeToBlack";
+import {PresentationLogo} from './components/PresentationLogo';
+import {Durations} from './Root';
+import {FadeToBlack} from './components/FadeToBlack';
 
 export const VideoInfoContext = React.createContext<Durations>({} as Durations);
 
 const MainVideo: React.FC<Durations> = (Durations) => {
     const {fps} = useVideoConfig();
-    
+    const frame = useCurrentFrame();
+
+    const outroStartFrame = fps * (Durations.total - Durations.outro);
+    const outroEndFrame = fps * Durations.total;
+
+    const volume = interpolate(
+        frame,
+        [outroStartFrame, outroEndFrame],
+        [0.2, 0],
+        {extrapolateLeft: 'clamp', extrapolateRight: 'clamp'}
+    );
+
     return (
         <AbsoluteFill style={{backgroundColor: '#000'}}>
             <PresentationLogo outroStart={9999} />
-            <Html5Audio src={staticFile('/music/Presentation.mp3')} trimBefore={220} volume={0.2} />
+            <Html5Audio
+                src={staticFile('/music/Presentation.mp3')}
+                trimBefore={220}
+                volume={volume}
+            />
             <VideoInfoContext.Provider value={Durations}>
                 <Sequence from={0} durationInFrames={fps * Durations.total}>
                     <Grid />
                 </Sequence>
-                <Sequence from={fps * (Durations.total - Durations.outro)} durationInFrames={fps * Durations.outro}>
+                <Sequence
+                    from={outroStartFrame}
+                    durationInFrames={fps * Durations.outro}
+                >
                     <FadeToBlack fadeDurationFrames={fps * Durations.outro} />
                 </Sequence>
             </VideoInfoContext.Provider>
@@ -27,4 +53,3 @@ const MainVideo: React.FC<Durations> = (Durations) => {
 };
 
 export default MainVideo;
-
